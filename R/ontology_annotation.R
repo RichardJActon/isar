68923160e7f672e8c6728984dd6b46c268fe805a
Richard James Acton <richard.acton@babraham.ac.uk>
2023-02-20 15:02:10 +0000

    check term & term_accession

--- R/ontology_annotation.R
+++ R/ontology_annotation.R
@@ -15,9 +15,9 @@
 ontology_annotation <- R6Class(
 	"ontology_annotation",
 	public = list(
-		term = '', # str
+		term = NULL, # str
 		term_source = NULL, # ontology_source
-		term_accession = '', # str
+		term_accession = NULL, # str
 		comments = list(), # comment
 		id = '', #
 
@@ -36,23 +36,36 @@ ontology_annotation <- R6Class(
 		#'     term_accession = "agreesWith"
 		#' )
 		initialize = function(
-			term = '', # str
+			term = NULL, # str
 			term_source = NULL, # ontology_source
-			term_accession = '', # str
+			term_accession = NULL, # str
 			comments = list(), # comment
 			id = '' #
 		) {
 			self$term_source <- term_source # ontology_source
-			if(!is.null(term_source)){
+			if(!is.null(term)) {
 				# handling on not explicitly enumerated lists?
 				# - check a remote resource with an API call?
 				if(term %in% names(self$term_source$terms_list)) {
 					self$term <- term # str
+					if (is.null(term_accession)) {
+						self$term_accession <- self$term_source$terms_list[[term]]
+					} else if (self$term_source$terms_list[[term]] != term_accession) {
+						stop("Supplied term & term accession do not match!")
+					}
 				} else {
 					stop("term is not in term source")
 				}
+			}
+			if(!is.null(term_accession)) {
 				if(term_accession %in% unlist(self$term_source$terms_list)) {
 					self$term_accession <- term_accession # str
+					term_vec <- unlist(self$term_source$terms_list)
+					if (is.null(term)) {
+						self$term <- names(term_vec[term_vec == term_accession])
+					} else if (names(term_vec[term_vec == term_accession]) != term) {
+						stop("Supplied term accession & term do not match!")
+					}
 				} else {
 					stop("term accession is not in the term source")
 				}
@@ -75,6 +88,10 @@ ontology_annotation <- R6Class(
 				"comments" = self$comments ## !!
 			)
 			return(ontology_annotation)
+		}
+
+		from_list = function(lst) {
+
 		}
 	)
 )
