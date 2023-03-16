b3b4131fe6b65bd863ee22bc05a47c5a947bac07
Richard James Acton <richard.acton@babraham.ac.uk>
2023-03-16 10:05:59 +0000

    Publication Object

---
+++ R/Publication.R
@@ -0,0 +1,15 @@
+#' R6 class for a Publication
+#'
+#' possible use of  https://docs.ropensci.org/rcrossref/index.html
+#'
+#'
+Publication <- R6::R6Class(
+	"Publication",
+	list(
+		initialize = function(
+
+		) {
+
+		}
+	)
+)
