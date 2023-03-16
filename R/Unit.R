2e54237a64a65efdec7011e3cb1d94f3a249fd5f
Richard James Acton <richard.acton@babraham.ac.uk>
2023-03-16 11:51:02 +0000

    Unit Notes

--- R/Unit.R
+++ R/Unit.R
@@ -1,5 +1,8 @@
-# alternative to ontology anotation when using a quantity not a controled vocab
-# value handling? - where to put the vaidation?
+# alternative to ontology annotation when using a quantity not a controlled vocab
+# value handling? - where to put the validation?
+#
+# have a warnings / messages field that can indicate if you are not using an ontology and
+# show this in the UI?
 
 # hard code units of measurement ontology?
 # unit annotation
