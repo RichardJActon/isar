f683df9c3003faeed191ef8e77ea8a25b4e73250
Richard James Acton <richard.acton@babraham.ac.uk>
2023-04-13 16:51:39 +0000

    datafile checks

--- isar/R/DataFile.R
+++ isar/R/DataFile.R
@@ -37,7 +37,7 @@ DataFile <- R6::R6Class(
 		#' @param generated_from Reference to Sample(s) the DataFile is generated from
 		#' @param comments Comments associated with instances of this class.
 		initialize = function(
-			filename = NULL,
+			filename = character(),
 			file_path = character(),
 			check_file_exists = FALSE,
 			compute_hash = FALSE,
@@ -46,18 +46,43 @@ DataFile <- R6::R6Class(
 			generated_from = NULL,
 			comments = NULL
 		) {
-			self$filename <- filename
+			if (checkmate::qtest(filename, "S[0]")) {
+				self$filename <- filename
+			} else {
+				self$set_filename(filename)
+			}
+
+			if (checkmate::qtest(file_path, "S[0]")) {
+				self$file_path <- file_path
+			} else {
+				self$set_file_path(file_path)
+			}
+			if (is.null(filename) && !is.null(self$file_path)) {
+				self$set_filename(fs::path_file(self$file_path))
+				message(paste0("Setting filename from path: ", self$filename))
+			}
+			if (!is.null(filename) && !is.null(self$file_path)) {
+				if (fs::path_file(self$file_path) != filename) {
+					stop("filename does not match path")
+				}
+			}
+			# self$file_path <- file_path
 
 			if(isTRUE(check_file_exists)) {
 				if(!fs::file_exists(file_path)) {
 					stop(paste0("Could not find: ", file_path))
 				}
 			}
-
-			self$file_path <- file_path
 			self$check_file_exists <- check_file_exists
+
+			if (isTRUE(compute_hash)) {
+				private$hash <- digest(
+					object = file_path, algo = hash_algo, file = TRUE
+				)
+			}
 			self$compute_hash <- compute_hash
 			self$hash_algo <- hash_algo
+
 
 			self$label <- label
 			if (is.null(generated_from)) {
@@ -67,6 +92,41 @@ DataFile <- R6::R6Class(
 			}
 			self$comments <- comments
 		},
+		#' @details
+		#' Check if the filename of the \code{[DataFile]} is a string
+		#' @param filename The filename of the \code{[DataFile]}
+		check_filename = function(filename) {
+			check <- checkmate::check_string(filename, min.chars = 1L)
+			error_with_check_message_on_failure(check)
+		},
+		#' @details
+		#' set the filename of the \code{[DataFile]} if valid
+		#' @param filename The filename of the \code{[DataFile]}
+		set_filename = function(filename) {
+			if (self$check_filename(filename)) { self$filename <- filename }
+		},
+
+		#' @details
+		#' Check if the file_path of the \code{[DataFile]} is a string
+		#' @param file_path The file_path of the \code{[DataFile]}
+		check_file_path = function(file_path) {
+			check <- checkmate::check_string(file_path, min.chars = 1L)
+			error_with_check_message_on_failure(check)
+		},
+		#' @details
+		#' set the file_path of the \code{[DataFile]} if valid
+		#' @param file_path The file_path of the \code{[DataFile]}
+		set_file_path = function(file_path) {
+			if (self$check_file_path(file_path)) { self$file_path <- file_path }
+		},
+
+		check_hash_algo = function() {
+			all(self$hash_algo %in% c(
+				"md5", "sha1", "crc32", "sha256", "sha512", "xxhash32",
+				"xxhash64", "murmur32", "spookyhash", "blake3"
+			))
+		},
+
 		#' @details
 		#' Check that generated_from is an \code{[Sample]} object
 		#' @param generated_from an \code{[Sample]} object
@@ -94,6 +154,20 @@ DataFile <- R6::R6Class(
 		set_comments = function(comments) {
 			if(self$check_comments(comments)) { self$comments <- comments }
 		},
+
+		#' @details
+		#'
+		#' Checks that the hash of the file at filepath matches the stored
+		#' hash of the file. Returns TRUE if they match and FALSE if not.
+		#'
+		#' validate_file
+		#' @return logical
+		validate_file = function() {
+			all(private$hash == digest(
+				object = self$file_path, algo = self$hash_algo, file = TRUE
+			))
+		},
+
 		#' @details
 		#' An R list representation of a \code{[DataFile]} object
 		#' @param ld linked data (default FALSE)
@@ -133,7 +207,8 @@ DataFile <- R6::R6Class(
 		}
 	),
 	private = list(
-		id = uuid::UUIDgenerate()
+		id = uuid::UUIDgenerate(),
+		hash = NULL
 	)
 )
 
