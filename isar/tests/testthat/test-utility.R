05f05c45c1571e75dc97d20abe767d725e937947
Richard James Acton <richard.acton@babraham.ac.uk>
2023-04-21 12:22:39 +0000

    Id utility function tests

--- isar/tests/testthat/test-utility.R
+++ isar/tests/testthat/test-utility.R
@@ -1,8 +1,10 @@
+# error_with_check_message_on_failure ----
 test_that("error_with_check_message_on_failure works", {
 	expect_error(error_with_check_message_on_failure("error text"), regexp = "error text")
 	expect_true(error_with_check_message_on_failure(TRUE))
 })
 
+# check_comments ----
 test_that("check_comments works", {
 	expect_true(check_comments(NULL))
 	expect_error(check_comments("string"), regexp = "Must be of type 'list' \\(or 'NULL'\\), not 'character'")
@@ -17,4 +19,23 @@ test_that("check_comments works", {
 	expect_error(check_comments(list("string")), regexp = "Must have names")
 	expect_true(check_comments(list(name = "string")))
 	expect_true(error_with_check_message_on_failure(TRUE))
+})
+
+
+# S3? ----
+
+# custom uuid functions ----
+test_that("generate_id, test_id & check_id work", {
+	id2test <- generate_id()
+	expect_true(uuid::UUIDvalidate(id2test))
+	expect_true(test_id(id2test))
+	id_with_suffix2test <- generate_id(suffix = "x")
+	expect_false(uuid::UUIDvalidate(id_with_suffix2test))
+	expect_true(test_id(id_with_suffix2test))
+	expect_error(generate_id(suffix = "-x"), regexp = "suffix must not contain any special characters")
+	expect_error(generate_id("notauuid"), regexp = "invalid uuid!")
+	expect_error(test_id(1), regexp = "input must be a character vector")
+	expect_equal(check_id("notauuid"), "id must be a valid uuid or a valid uuid with a suffix")
+	expect_true(check_id(id2test))
+	expect_true(check_id(id_with_suffix2test))
 })
