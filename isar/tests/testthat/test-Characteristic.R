27e4125317841671e61f37d9d120341cb5567906
Richard James Acton <richard.acton@babraham.ac.uk>
2023-04-21 12:24:36 +0000

    characteristic tests - unfinished

--- isar/tests/testthat/test-Characteristic.R
+++ isar/tests/testthat/test-Characteristic.R
@@ -1,3 +1,4 @@
+# Characteristic ----
 test_that("Characteristic works", {
 
 	drug <- OntologySource$new("drug", terms_list = list(dox = "dox", flox = "flox", pox = "pox"))
@@ -12,7 +13,7 @@ test_that("Characteristic works", {
 	)
 	checkmate::expect_r6(test_characteristic, "Characteristic")
 
-	# Comments
+	## Comments ----
 	expect_true(test_characteristic$check_comments(list("a" = "b")))
 	expect_true(test_characteristic$check_comments(list("a" = "1", "b" = "2")))
 
@@ -23,13 +24,51 @@ test_that("Characteristic works", {
 	expect_error(test_characteristic$set_comments(list("b")), regexp = "Must have names")
 	expect_error(test_characteristic$set_comments("b"), regexp = "Must be of type 'list' \\(or 'NULL'\\), not 'character'")
 
-	#
+	## ID ----
 	expect_true(uuid::UUIDvalidate(test_characteristic$get_id()))
 
 	test_characteristic$set_comments(list("a" = "1", "b" = "2"))
-	test_characteristic$to_list()
+	# test_characteristic$to_list(recursive = TRUE)
+
+	## To list ----
+	# !! test recursive
+	test_characteristic <- Characteristic$new()
+	test_characteristic$set_category(exposure_1)
+	test_characteristic$set_unit(mg)
+	test_characteristic$set_value(10)
+
+
+	example_list <- list(
+		id = test_characteristic$get_id(),
+		category = "dox",
+		value = 10,
+		unit = "milligram",
+		comments = NULL
+	)
+	expect_equal(test_characteristic$to_list(recursive = FALSE), example_list)
+
+	## From list ----
+	# !! test recursive
+	test_from_list <- OntologyAnnotation$new()
+	test_from_list$from_list(example_list, recursive = FALSE)
+	# expect_equal(test_from_list$to_list(recursive = FALSE), example_list)
+
+	test_from_list <- Characteristic$new(
+		category = exposure_1, value = 10, unit = mg
+	)
+
+	example_list_null <- list(
+		id = test_characteristic$get_id(),
+		annotation_value = NULL,
+		term_source = NULL,
+		term_accession = NULL,
+		comments = NULL
+	)
+	test_from_list <- Characteristic$new()
+	test_from_list$from_list(example_list_null, recursive = FALSE)
+	# expect_equal(test_from_list$to_list(recursive = FALSE), example_list_null)
 
 	# identity tests
-	expect_true(identical(test_characteristic, test_characteristic))
-	expect_false(identical(test_characteristic, Characteristic$new()))
+	# expect_true(identical(test_characteristic, test_characteristic))
+	# expect_false(identical(test_characteristic, Characteristic$new()))
 })
