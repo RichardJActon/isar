92ecc01facdc0bdce73ef432f4a784e55bc51aba
Richard James Acton <richard.acton@babraham.ac.uk>
2023-05-19 12:53:05 +0000

    Add CRediT Taxonomy Ontology, document

--- isar/R/data.R
+++ isar/R/data.R
@@ -1,4 +1,79 @@
 #' Ontology of Units of Measure
-#' @format \code{[OntologySource]} object
+#' @format [OntologySource] object
 #' @source <https://raw.githubusercontent.com/HajoRijgersberg/OM/master/om-2.0.rdf>
 "OM"
+
+#' CRediT taxonomy
+#' @format [OntologySource] object
+#' @source <https://credit.niso.org/>
+"CRediT"
+
+#' CRediT taxonomy: Conceptualization
+#' @format [OntologyAnnotation] object
+#' @source <https://credit.niso.org/contributor-roles/conceptualization/>
+"Conceptualization"
+
+#' CRediT taxonomy: Data curation
+#' @format [OntologyAnnotation] object
+#' @source <https://credit.niso.org/contributor-roles/data-curation/>
+"Data curation"
+
+#' CRediT taxonomy: Formal analysis
+#' @format [OntologyAnnotation] object
+#' @source <https://credit.niso.org/contributor-roles/formal-analysis/>
+"Formal analysis"
+
+#' CRediT taxonomy: Funding acquisition
+#' @format [OntologyAnnotation] object
+#' @source <https://credit.niso.org/contributor-roles/funding-acquisition/>
+"Funding acquisition"
+
+#' CRediT taxonomy: Investigation
+#' @format [OntologyAnnotation] object
+#' @source <https://credit.niso.org/contributor-roles/investigation/>
+"Investigation"
+
+#' CRediT taxonomy: Methodology
+#' @format [OntologyAnnotation] object
+#' @source <https://credit.niso.org/contributor-roles/methodology/>
+"Methodology"
+
+#' CRediT taxonomy: Project administration
+#' @format [OntologyAnnotation] object
+#' @source <https://credit.niso.org/contributor-roles/project-administration/>
+"Project administration"
+
+#' CRediT taxonomy: Resources
+#' @format [OntologyAnnotation] object
+#' @source <https://credit.niso.org/contributor-roles/resources/>
+"Resources"
+
+#' CRediT taxonomy: Software
+#' @format [OntologyAnnotation] object
+#' @source <https://credit.niso.org/contributor-roles/software/>
+"Software"
+
+#' CRediT taxonomy: Supervision
+#' @format [OntologyAnnotation] object
+#' @source <https://credit.niso.org/contributor-roles/supervision/>
+"Supervision"
+
+#' CRediT taxonomy: Validation
+#' @format [OntologyAnnotation] object
+#' @source <https://credit.niso.org/contributor-roles/validation/>
+"Validation"
+
+#' CRediT taxonomy: Visualization
+#' @format [OntologyAnnotation] object
+#' @source <https://credit.niso.org/contributor-roles/visualization/>
+"Visualization"
+
+#' CRediT taxonomy: Writing - original draft
+#' @format [OntologyAnnotation] object
+#' @source <https://credit.niso.org/contributor-roles/writing-original-draft/>
+"Writing - original draft"
+
+#' CRediT taxonomy: Writing - review & editing
+#' @format [OntologyAnnotation] object
+#' @source <https://credit.niso.org/contributor-roles/writing-review-editing/>
+"Writing - review & editing"
