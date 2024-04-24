d02c99bfb7c402500570619b12a856705acadb22
Richard James Acton <richard.acton@babraham.ac.uk>
2024-04-24 11:51:43 +0000

    read some example tables in isatab testing (comparing to json whilst working on json processSequence parsing)

--- isar/tests/testthat/test-isatab-parser.R
+++ isar/tests/testthat/test-isatab-parser.R
@@ -1,10 +1,27 @@
+# BII_I_1_tab <- readr::read_tsv(
+# 	fs::path_abs(fs::path_join(c(
+# 		testthat::test_path(), "test-data/ISAdatasets/tab/BII-I-1/i_investigation.txt"
+# 	)))
+# )
+BII_S_3_tab <- readr::read_tsv(
+	fs::path_abs(fs::path_join(c(
+		testthat::test_path(), "test-data/ISAdatasets/tab/BII-S-3/s_BII-S-3.txt"
+	)))
+
+)
+BII_S_3_assay_Gx_tab <- readr::read_tsv(
+	fs::path_abs(fs::path_join(c(
+		testthat::test_path(), "test-data/ISAdatasets/tab/BII-S-3/a_gilbert-assay-Gx.txt"
+	)))
+
+)
 # ISA-tab directory validation works ----
 test_that("ISA-tab directory validation works", {
 	## Directory exists ----
 	isa_tab_example_dir <- paste0("/not/a/directory", uuid::UUIDgenerate())
 	expect_error(
-		isa_tab_example_dir %>% 
-			check_isa_tab_dir() %>% 
+		isa_tab_example_dir %>%
+			check_isa_tab_dir() %>%
 			error_with_check_message_on_failure()
 	)
 	## There are files in the directory ----
@@ -26,7 +43,7 @@ test_that("ISA-tab directory validation works", {
 		fs::file_temp(pattern = "a_assay", tmp_dir = isa_tab_example_dir, ext = "txt")
 	)
 	purrr::walk(temp_files, fs::file_touch)
-	
+
 	## ISA-tab files have the right file extension ----
 	expect_error(
 		isa_tab_example_dir %>%
@@ -43,16 +60,16 @@ test_that("ISA-tab directory validation works", {
 		fs::file_temp(pattern = "a_assay", tmp_dir = isa_tab_example_dir, ext = "txt")
 	)
 	purrr::walk(temp_files, fs::file_touch)
-	
+
 	expect_error(
 		isa_tab_example_dir %>%
 			check_isa_tab_dir() %>%
 			error_with_check_message_on_failure(),
 		regexp = "More than one Investigation file found\\!"
 	)
-	
+
 	purrr::walk(fs::dir_ls(isa_tab_example_dir), fs::file_delete)
-	
+
 	## At leasts of Study and Assay files ----
 	temp_files <- c(
 		fs::file_temp(pattern = "i_investigation", tmp_dir = isa_tab_example_dir, ext = "txt"),
@@ -60,27 +77,27 @@ test_that("ISA-tab directory validation works", {
 		fs::file_temp(pattern = "a_assay", tmp_dir = isa_tab_example_dir, ext = "txt")
 	)
 	purrr::walk(temp_files, fs::file_touch)
-	
+
 	expect_error(
 		isa_tab_example_dir %>%
 			check_isa_tab_dir() %>%
 			error_with_check_message_on_failure(),
 		regexp = "There must be at least 1 investigation, 1 study, and 1 assay"
 	)
-	
+
 	purrr::walk(fs::dir_ls(isa_tab_example_dir), fs::file_delete)
 })
 
 test_that("get_isatab_files works", {
 	isa_tab_example_dir <- fs::path_temp()
-	
+
 	temp_files <- c(
 		fs::file_temp(pattern = "i_investigation", tmp_dir = isa_tab_example_dir, ext = "txt"),
 		fs::file_temp(pattern = "s_study", tmp_dir = isa_tab_example_dir, ext = "txt"),
 		fs::file_temp(pattern = "a_assay", tmp_dir = isa_tab_example_dir, ext = "txt")
 	)
 	purrr::walk(temp_files, fs::file_touch)
-	
+
 	expect_message(
 		files_tab <- get_isatab_files(isa_tab_example_dir, verbose = TRUE),
 		regexp = "1 Investigation"
@@ -91,8 +108,8 @@ test_that("get_isatab_files works", {
 		factor(c("i","s","a"), levels = c("i","s","a"), ordered = TRUE),
 		files_tab$type
 	)
-	
+
 	purrr::walk(temp_files, fs::file_delete)
-	
+
 	# Need more negative test cases
 })
