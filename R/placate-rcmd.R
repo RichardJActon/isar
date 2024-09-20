32db02974bcda2b8e0a401875ac7c2d91a88af46
Richard James Acton <richard.acton@babraham.ac.uk>
2024-09-20 09:15:52 +0000

    use globalVAriables to placate R CMD no bindings for global variables

---
+++ R/placate-rcmd.R
@@ -0,0 +1,17 @@
+globalVariables(c(
+	".",
+	"X1",
+	"rowname",
+	"vname",
+	"x",
+	"paths",
+	"files",
+	"type",
+	"n",
+	"section",
+	"section_index",
+	"isa_tab_section_headings",
+	"data",
+	"sort_list_by_name",
+	"PubMed ID"
+))
