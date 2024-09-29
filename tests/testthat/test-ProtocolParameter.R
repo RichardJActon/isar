09d1bb49b2dc7f1c7ea4cb328dc4d85c55c908b0
Richard James Acton <richard.acton@babraham.ac.uk>
2024-09-29 23:33:40 +0000

    update protocol parameter tests

--- tests/testthat/test-ProtocolParameter.R
+++ tests/testthat/test-ProtocolParameter.R
@@ -5,34 +5,42 @@ test_that("ProtocolParameter works", {
 	## Parameter Name ----
 	expect_null(test_protocol_parameter$parameter_name)
 
-	oa <- OntologyAnnotation$new(term = "gram", term_source = OM)
-	test_protocol_parameter$set_parameter_name(oa)
+	warns <- capture_warnings(
+		oa <- OntologyAnnotation$new(term = "gram", term_source = OM)
+	)
+	
+	expect_match(warns, "Attempting to add it", all = FALSE)
+	warns <- capture_warnings(test_protocol_parameter$set_parameter_name(oa))
+	expect_match(warns, "Attempting to add it", all = FALSE)
 	checkmate::expect_r6(test_protocol_parameter$parameter_name, "OntologyAnnotation")
 
 	## Comments ----
 	test_comments(test_protocol_parameter)
 
-	## ID ----
-	# expect_true(uuid::UUIDvalidate(test_protocol_parameter$get_id()))
-
 	## To list ----
 	example_list <- list(
-		id = test_protocol_parameter$get_id(),
-		parameter_name = "gram",
-		comments = NULL
+		parameterName = list(
+			termAccession = "g",
+			annotationValue = "gram",
+			termSource = "Ontology of units of Measure (OM)"
+		),
+		comments = NULL,
+		`@id` = character()
 	)
-	expect_equal(test_protocol_parameter$to_list(recursive = FALSE), example_list)
+	expect_equal(test_protocol_parameter$to_list(), example_list)
 
 	## From list ----
+	# without OM in the reference
 	test_from_list <- ProtocolParameter$new()
-	test_from_list$from_list(example_list, recursive = FALSE)
-	expect_equal(test_from_list$to_list(recursive = FALSE), example_list)
+	warns <- capture_warnings(test_from_list$from_list(example_list))
+	expect_match(warns, "Term Source Unknown")
+	expect_equal(test_from_list$to_list(), example_list)
+	
+	# with OM in the reference
+	ors <- OntologySourceReferences$new()
+	ors$add_ontology_source(OM)
+	test_from_list <- ProtocolParameter$new(ontology_source_references = ors)
+	test_from_list$from_list(example_list)
+	expect_equal(test_from_list$to_list(), example_list)
 
-	## Identity ----
-
-	# pp1 <- ProtocolParameter$new()
-	# pp1a <- pp1
-	# pp2 <- ProtocolParameter$new()
-	# expect_true(identical(pp1, pp1a))
-	# expect_false(identical(pp1, pp2))
 })
