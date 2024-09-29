90039395089bb1f108656361b9c33a0b2be401a7
Richard James Acton <richard.acton@babraham.ac.uk>
2024-09-29 23:33:00 +0000

    update protocol tests

--- tests/testthat/test-Protocol.R
+++ tests/testthat/test-Protocol.R
@@ -12,33 +12,51 @@ test_that("Protocol works", {
 	
 	## To list ----
 	example_list <- list(
-		name = "DNA isolation",
-		id = test_protocol$get_id(),
-		protocol_type = NULL,
-		description = character(),
+		parameters = list(),
+		components = list(),
 		uri = character(),
+		description = character(),
 		version = character(),
-		parameters = NULL,
-		components = NULL,
-		comments = NULL
+		`@id` = "#protocol/",
+		name = "DNA isolation",
+		# protocolType = list()# ,
+		protocolType = list(annotationValue = "")# ,
+		# comments = NULL
 	)
-	expect_equal(test_protocol$to_list(recursive = FALSE), example_list)
+	expect_equal(test_protocol$to_list(), example_list)
 
 	## From list ----
 	test_from_list <- Protocol$new()
-	test_from_list$from_list(example_list, recursive = FALSE)
-	expect_equal(test_from_list$to_list(recursive = FALSE), example_list)
-
+	
+	warns <- capture_warnings(test_from_list$from_list(example_list))
+	expect_match(warns, "Unspecified Term", all = FALSE)
+	expect_match(warns, "Term Source Unknown", all = FALSE)
+	expect_match(warns, "Term not in source", all = FALSE)
+	
+	expect_equal(test_from_list$to_list(), example_list)
+	
 	example_list_null <- list(
-		name = "DNA isolation",
-		id = test_protocol$get_id(),
-		component_type = NULL,
+		parameters = NULL,
+		components = NULL,
+		uri = NULL,
+		description = NULL,
+		version = NULL,
+		`@id` = NULL,
+		name = NULL,
+		protocolType = NULL, 
 		comments = NULL
 	)
-	test_from_list <- ProtocolComponent$new()
-	test_from_list$from_list(example_list_null, recursive = FALSE)
-	expect_equal(test_from_list$to_list(recursive = FALSE), example_list_null)
-
-
-
+	test_from_list <- Protocol$new()
+	warns <- capture_warnings(test_from_list$from_list(example_list_null))
+	expect_match(warns, "Unspecified Term", all = FALSE)
+	expect_match(warns, "Term Source Unknown", all = FALSE)
+	expect_match(warns, "Term not in source", all = FALSE)
+	
+	expect_null_out_lst <- list(
+		parameters = list(),
+		components = list(),
+		protocolType = list(annotationValue = "")
+	)
+	expect_equal(test_from_list$to_list(), expect_null_out_lst)
+	
 })
