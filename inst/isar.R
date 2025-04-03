36f2318b9b098c9e1433256ec7a3b6103ff85a2f
Richard J. Acton <richardjacton@richardjacton.net>
2025-04-03 16:58:51 +0100

    basic test of cli reading investigation json

--- inst/isar.R
+++ inst/isar.R
@@ -1,17 +1,27 @@
 #! /usr/bin/env Rscript
-library(isar)
-library(optparse)
+suppressPackageStartupMessages({
+	library(isar)
+	library(optparse)
+})
 option_list <- list(
 	make_option(
-		c("-", "--"),
-		default = "",
-		help = "",
-		type = "",
-		dest = "",
-		action = ""
+		opt_str = c("--read-json-investigation", "-j"),
+		# default = " %default",
+		help = "Read a json representation of an ISA Investigation",
+		type = "character",
+		# dest = "", name in the options list of the stored value
+		action = "store" # store, store_true, store_false, callback
+		# callback = function()
 	)
 )
-parse_args2(
-	OptionParser(option_list = option_list),
-	args = c("--help")
+args <- parse_args2(
+	OptionParser(option_list = option_list)# , args = c("--help")
 )
+if (!is.null(args$options$read_json_investigation)) {
+	if(fs::file_exists(args$options$read_json_investigation)) {
+		obj <- Investigation$new()
+		# print(args)
+		obj$from_json(args$options$read_json_investigation)
+		obj
+	}
+}
