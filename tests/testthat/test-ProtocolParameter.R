0237c9dd481506e6741001eff26f58c9d690e989
Richard J. Acton <richardjacton@richardjacton.net>
2025-09-25 16:59:13 +0100

    test ProtocolParameter with example that has missing parameter name info

--- tests/testthat/test-ProtocolParameter.R
+++ tests/testthat/test-ProtocolParameter.R
@@ -11,10 +11,11 @@ test_that("ProtocolParameter works", {
 	warns <- capture_warnings(
 		oa <- OntologyAnnotation$new(term = "gram", term_source = OM)
 	)
-
 	expect_match(warns, "Attempting to add it", all = FALSE)
+
 	warns <- capture_warnings(test_protocol_parameter$set_parameter_name(oa))
 	expect_match(warns, "Attempting to add it", all = FALSE)
+
 	checkmate::expect_r6(test_protocol_parameter$parameter_name, "OntologyAnnotation")
 
 	## Comments ----
@@ -27,12 +28,26 @@ test_that("ProtocolParameter works", {
 			annotationValue = "gram",
 			termSource = "Ontology of units of Measure (OM)"
 		),
-		comments = NULL,
+		# comments = NULL,
 		`@id` = character()
 	)
 	expect_equal(test_protocol_parameter$to_list(), example_list)
 
-	## From list ----
+	test_from_example_list <- ProtocolParameter$new()
+	warns <- capture_warnings(test_from_example_list$from_list(example_list))
+
+	example_list_missing <- list(
+		parameterName = list(
+			annotationValue = "gram"
+		),
+		# comments = NULL,
+		`@id` = character()
+	)
+	test_from_list_missing <- ProtocolParameter$new()
+	warns <- capture_warnings(test_from_list_missing$from_list(example_list_missing))
+	expect_equal(test_from_list_missing$to_list(), example_list_missing)
+
+	#From list ----
 	# without OM in the reference
 	test_from_list <- ProtocolParameter$new()
 	warns <- capture_warnings(test_from_list$from_list(example_list))
